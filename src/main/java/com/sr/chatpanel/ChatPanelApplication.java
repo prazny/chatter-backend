197c55bc0ca3b3a782a88932e4677be13ebef100
Patryk Razny <kontakt@patryk-razny.pl>
2023-05-06 15:47:06 +0200

    Init

---
+++ src/main/java/com/sr/chatpanel/ChatPanelApplication.java
@@ -0,0 +1,13 @@
+package com.sr.chatpanel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatPanelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatPanelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sr/chatpanel/ChatPanelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sr.chatpanel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatPanelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
