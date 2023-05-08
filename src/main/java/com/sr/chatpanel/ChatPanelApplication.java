7ca21a22191eb2abf1dbd3c99352df3dbd9514ba
Patryk Razny <kontakt@patryk-razny.pl>
2023-05-08 20:40:30 +0200

    add cors config

--- src/main/java/com/sr/chatpanel/ChatPanelApplication.java
+++ src/main/java/com/sr/chatpanel/ChatPanelApplication.java
@@ -2,12 +2,32 @@ package com.sr.chatpanel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class ChatPanelApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ChatPanelApplication.class, args);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOriginPattern(CorsConfiguration.ALL);
+        configuration.setAllowedMethods(List.of(CorsConfiguration.ALL));
+        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
--- src/main/java/com/sr/chatpanel/models/Site.java
+++ src/main/java/com/sr/chatpanel/models/Site.java
@@ -4,20 +4,20 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.*;
 
 @Entity
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "site")
+@Getter
+@Setter
 public class Site {
     @Id
     private Long id;
+    @Column(name = "uri")
+    private String uri;
 
-    @Column(name = "url")
-    private String url;
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
 }
